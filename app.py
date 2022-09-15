efced274a13153c9783730cede77551413e96c85
sj-dev94 <sethjonesdevelopment@gmail.com>
2022-09-15 10:52:07 -0400

    building out classes

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-class card:
+
+#Creates a card object with a suit and a rank
+#adds method to combine suit and rank
+class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank 
+           
+    def __str__(self):
+        return self.rank + 'of' + self.suit
 
-class deck:
-    def __init__
+#Creates a deck object 
+class Deck:
+    def __init__(self):
+        self.deck = []
+        #outer loop that loops over suits
+        for suit in suits:
+            #inner loop that loops over ranks
+            for rank in ranks:
+                #uses the outer and inner loop to append the cards to the deck array
+                self.deck.append(Card(suit,rank))
+    def __str__(self):
+        #holds all of the cards in the deck
+        composition = ''
+        for card in self.deck:
+            composition += card.__str__()
+        return 'Card Deck: ' + composition
+    
+deck1 = Deck()
+print(deck1)
